b557fbdf9b058f80e594d4d753e5d46aafc4ad25
ReinisSprogis <69913791+ReinisSprogis@users.noreply.github.com>
2023-03-26 21:57:59 +0100

    Quickfix for Empty space hover. Returned full file

--- hover.js
+++ hover.js
@@ -30,14 +30,17 @@ function hoverInfoActivate(context) {
                 });
             };
            
+            if (word.length > 5) {
+                return; // Return early if the word is empty or whitespace
+            }
 
             //If the word is G0 or G00 or G1 or G01 or G001
             //Then return the G0/G1 page
             //This is because the G0/G1 page contains information on both G0 and G1
-            //This will be the same exclusive fore other hover elements that file name doesn't match the G/M code hovered.
+            //This will be the same exclusive for other keywords.
             //For example, the G2/G3 page contains information on both G2 and G3
-            //Other commands will be processed as if G4 is the same as G04 G004 and G0004 and so on.
-            //The same goes for M commands.
+            //Other commands will be processed as if G4 is the same as G04 G004 and G004.
+            //The same for M commands.
             //If no match is found, then no markup and hover info are shown. M29 M34 M665 M666 
             //Need to solve a way for G029 like files where multiple files are needed to be read for the same keyword.
 
@@ -55,7 +58,7 @@ function hoverInfoActivate(context) {
                 return getMarkdownFromFile(gcodeFile);
             }
             //G54 G55 G56 G57 G58 G59
-            else if(word == 'G54' || word == 'G55' || word == 'G56' || word == 'G57' || word == 'G58'|| word == 'G59' || word == 'G054' || word == 'G055' || word == 'G056' || word == 'G057' || word == 'G058'|| word == 'G059'){
+            else if(word == 'G54' || word == 'G55' || word == 'G56' || word == 'G57' || word == 'G58'|| word == 'G59' || word == 'G054' || word == 'G055' || word == 'G056' || word == 'G057' || word == 'G058'|| word == 'G059' || word == 'G59.1' || word == 'G59.2' || word == 'G59.3'){
                 const gcodeFile = path.join(__dirname, 'MarlinDocumentation-master', '_gcode', 'G054-G059.md');
                 return getMarkdownFromFile(gcodeFile);
             //M0 M1
@@ -79,20 +82,15 @@ function hoverInfoActivate(context) {
                 const gcodeFile = path.join(__dirname, 'MarlinDocumentation-master', '_gcode', 'M860-M869.md');
                 return getMarkdownFromFile(gcodeFile);
             //T1 T2    
-            }else if (word == 'T1' ||  word == 'T01' || word == 'T001' || word == 'T2' || word == 'T02' || word == 'T002'){
-                const gcodeFile = path.join(__dirname, 'MarlinDocumentation-master', '_gcode', 'T1-T2.md');
+            }else if (/T\d+/.test(word)){
+                console.log('T found:' + word);
+                const gcodeFile = path.join(__dirname, 'MarlinDocumentation-master', '_gcode', 'T001-T002.md');
                 return getMarkdownFromFile(gcodeFile);
             }
             else {
-                //If G or M value contains 1 or 2 digits, add the 0 at the beginning to match 3 digit format.
-                //G1 -> G001
-                //G01 -> G001
-                //G001 -> G001
-                //M1 -> M001
-                //M01 -> M001
-                //M001 -> M001
+               
                 filtered = '';
-                if (word[0] == 'G' || word[0] == 'M' && word.length <= 3) {
+                if (word[0] == 'G' || word[0] == 'M'  && word.length <= 3) {
                     if (word.length == 2) {
                         filtered = word[0] + '00' + word.substring(1);
                     } else if (word.length == 3) {
