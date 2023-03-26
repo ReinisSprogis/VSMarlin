407a22a254e5d6e7790f8474849129925cffe625
ReinisSprogis <69913791+ReinisSprogis@users.noreply.github.com>
2023-03-26 18:19:48 +0100

    Migrated from web repo to local

--- hover.js
+++ hover.js
@@ -1,98 +1,89 @@
 const vscode = require('vscode');
-const axios = require('axios');
-const cheerio = require('cheerio');
+const fs = require('fs');
 const path = require('path');
+const MarkdownIt = require('markdown-it');
+const md = new MarkdownIt();
 
 //This function provides information on hovering over keywords
 function hoverInfoActivate(context) {
     console.log('hoverInfoActivate is activated!');
-    const keywords = [
-        'G01', 'G1', 'G001'
-    ];
 
     const hoverProvider = vscode.languages.registerHoverProvider('marlin', {
         provideHover(document, position, token) {
             const range = document.getWordRangeAtPosition(position);
             const word = document.getText(range);
-            const baseURL = "https://marlinfw.org";
-            const relativeUrlRegex = /<a href="(\/.*?)">/g;
             console.log("Word: " + word);
+            
+
+            const getMarkdownFromFile = (filePath) => {
+                return new Promise((resolve, reject) => {
+                    fs.readFile(filePath, 'utf8', (err, data) => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            const markdownString = md.render(data);
+                            const hoverMarkdownString = new vscode.MarkdownString(markdownString, true);
+                            hoverMarkdownString.supportHtml = true;
+                            resolve(new vscode.Hover(hoverMarkdownString));
+                        }
+                    });
+                });
+            };
+           
 
             //If the word is G0 or G00 or G1 or G01 or G001
             //Then return the G0/G1 page
             //This is because the G0/G1 page contains information on both G0 and G1
-            //Linear movement commands
+            //This will be the same exclusive fore other hover elements that file name doesn't match the G/M code hovered.
+            //For example, the G2/G3 page contains information on both G2 and G3
+            //Other commands will be processed as if G4 is the same as G04 G004 and G0004 and so on.
+            //The same goes for M commands.
+            //If no match is found, then no markup and hover info are shown. M29 M34 M665 M666 
+            //Need to solve a way for G029 like files where multiple files are needed to be read for the same keyword.
+
+            //G0/G1
             if (word == 'G0' || word == 'G00' || word == 'G1' || word == 'G01' || word == 'G001') {
-                console.log("G0/G1")
-                return axios.get('https://marlinfw.org/docs/gcode/G000-G001.html')
-                    .then(response => {
-                        const data = cheerio.load(response.data);
-                        const content = data('div.col-lg-12.row.long').html();
-
-                        console.log("Content g1: " + content);
-                        const updatedMarkdownContent = content.toString().replace(relativeUrlRegex, `<a href="${baseURL}$1">`);
-                        const markdownString = new vscode.MarkdownString('' + updatedMarkdownContent, true);
-                        markdownString.supportHtml = true;
-                        return new vscode.Hover(markdownString);
-                    })
-                    .catch(error => {
-                        console.error(error);
-                    });
-
+                const gcodeFile = path.join(__dirname, 'MarlinDocumentation-master', '_gcode', 'G000-G001.md');
+              return  getMarkdownFromFile(gcodeFile);
+            //G2/G3
             } else if (word == 'G2' || word == 'G3' || word == 'G02' || word == 'G03' || word == 'G002' || word == 'G003') {
-                //If the word is G2 or G3 or G02 or G03 or G002 or G003
-                //Then return the G2/G3 page
-                //This is because the G2/G3 page contains information on both G2 and G3
-                //Circular movement commands
-
-
-                return axios.get('https://marlinfw.org/docs/gcode/G002-G003.html')
-                    .then(response => {
-                        const data = cheerio.load(response.data);
-                        const content = data('div.col-lg-12.row.long').html();
-
-                        console.log("Content g2: " + content);
-                        const markdownString = new vscode.MarkdownString('' + content.toString(), true);
-                        markdownString.supportHtml = true;
-                        return new vscode.Hover(markdownString);
-                    })
-                    .catch(error => {
-                        console.error(error);
-                    });
+                    const gcodeFile = path.join(__dirname, 'MarlinDocumentation-master', '_gcode', 'G002-G003.md');
+                    return getMarkdownFromFile(gcodeFile);
+            //G17 G18 G19
             } else if (word == 'G17' || word ==  'G18' || word ==  'G19' || word ==  'G017' || word ==   'G018' || word ==  'G019') {
-                //If the word is G17, G18, G19, G017, G018, G019
-                //Then return the G17/G19 page
-                //This is because the G17/G19 page contains information on both G17 and G19
-                //Plane selection commands
-                return axios.get('https://marlinfw.org/docs/gcode/G017-G019.html')
-                    .then(response => {
-                        const data = cheerio.load(response.data);
-                        const content = data('div.col-lg-12.row.long').html();
-                        console.log("Content g17: " + content);
-                        const markdownString = new vscode.MarkdownString('' + content.toString(), true);
-                        markdownString.supportHtml = true;
-                        return new vscode.Hover(markdownString);
-                    })
+                const gcodeFile = path.join(__dirname, 'MarlinDocumentation-master', '_gcode', 'G017-G019.md');
+                return getMarkdownFromFile(gcodeFile);
             }
+            //G54 G55 G56 G57 G58 G59
             else if(word == 'G54' || word == 'G55' || word == 'G56' || word == 'G57' || word == 'G58'|| word == 'G59' || word == 'G054' || word == 'G055' || word == 'G056' || word == 'G057' || word == 'G058'|| word == 'G059'){
-                //If the word is G54, G55, G56, G57, G58, G59, G054, G055, G056, G057, G058, G059
-                //Then return the G54/G59 page
-                //This is because the G54/G59 page contains information on both G54 and G59
-                //Coordinate system selection commands
-                return axios.get('https://marlinfw.org/docs/gcode/G054-G059.html')
-                    .then(response => {
-                        const data = cheerio.load(response.data);
-                        const content = data('div.col-lg-12.row.long').html();
-                        console.log("Content g54: " + content);
-                        const markdownString = new vscode.MarkdownString('' + content.toString(), true);
-                        markdownString.supportHtml = true;
-                        return new vscode.Hover(markdownString);
-                    })
-            
-                
+                const gcodeFile = path.join(__dirname, 'MarlinDocumentation-master', '_gcode', 'G054-G059.md');
+                return getMarkdownFromFile(gcodeFile);
+            //M0 M1
+            }else if(word == 'M0' || word == 'M00' || word == 'M000' || word == 'M1' || word == 'M01' || word == 'M001'){
+                const gcodeFile = path.join(__dirname, 'MarlinDocumentation-master', '_gcode', 'M000-M001.md');
+                return getMarkdownFromFile(gcodeFile);
+            //M7 M8 M9
+            }else if(word == 'M7' || word == 'M07' || word == 'M007' || word == 'M8' || word == 'M08' || word == 'M008'|| word == 'M9' || word == 'M09' || word == 'M009'){
+                const gcodeFile = path.join(__dirname, 'MarlinDocumentation-master', '_gcode', 'M007-M009.md');
+                return getMarkdownFromFile(gcodeFile);
+            //M10 M11
+            }else if (word == 'M10' || word == 'M010' || word == 'M11' || word == 'M011'){
+                const gcodeFile = path.join(__dirname, 'MarlinDocumentation-master', '_gcode', 'M010-M011.md');
+                return getMarkdownFromFile(gcodeFile);
+            //M810 M811 M812 M813 M814 M815 M816 M817 M818 M819 
+            }else if (word == 'M810' || word == 'M811' || word == 'M812' || word == 'M813' || word == 'M814' || word == 'M815' || word == 'M816' || word == 'M817' || word == 'M818' || word == 'M819'){
+                const gcodeFile = path.join(__dirname, 'MarlinDocumentation-master', '_gcode', 'M810-M819.md');
+                return getMarkdownFromFile(gcodeFile);
+            //M860, M861, M862, M863, M864, M865, M866, M867, M868, M869    
+            }else if (word == 'M860' || word == 'M861' || word == 'M862' || word == 'M863' || word == 'M864' || word == 'M865' || word == 'M866' || word == 'M867' || word == 'M868' || word == 'M869'){
+                const gcodeFile = path.join(__dirname, 'MarlinDocumentation-master', '_gcode', 'M860-M869.md');
+                return getMarkdownFromFile(gcodeFile);
+            //T1 T2    
+            }else if (word == 'T1' ||  word == 'T01' || word == 'T001' || word == 'T2' || word == 'T02' || word == 'T002'){
+                const gcodeFile = path.join(__dirname, 'MarlinDocumentation-master', '_gcode', 'T1-T2.md');
+                return getMarkdownFromFile(gcodeFile);
             }
             else {
-
                 //If G or M value contains 1 or 2 digits, add the 0 at the beginning to match 3 digit format.
                 //G1 -> G001
                 //G01 -> G001
@@ -112,44 +103,9 @@ function hoverInfoActivate(context) {
                 } else {
                     filtered = word;
                 }
-
-                console.log("Filtered: " + filtered);
-                return axios.get('https://marlinfw.org/docs/gcode/' + filtered + '.html')
-                    .then(response => {
-                        const data = cheerio.load(response.data);
-                        const description = data('div.col-lg-12.row.long').html();
-                        const notes = data('div.col-lg-12.row.notes').html();
-                        const usage = data('div.col-lg-12.row.usage').html();
-                        const params = data('div.col-lg-12.row.params').html();
-                        const examples = data('div.col-lg-12.row.examples').html();
-                        console.log("examples: " + examples);
-                        const markdownString = new vscode.MarkdownString();
-                        console.log("Content: " + description);
-                        if(description != null){
-                         markdownString.appendMarkdown('' + description.toString(), true);
-                        }
-                        if(notes != null){
-                        markdownString.appendMarkdown('_______________________________________________________________');
-                        markdownString.appendMarkdown(notes,true);
-                        }
-                        if(usage != null){
-                        markdownString.appendMarkdown('_______________________________________________________________');
-                        markdownString.appendMarkdown(usage.toString(), true);
-                        }
-                        if(params != null){
-                        markdownString.appendMarkdown('_______________________________________________________________');
-                        markdownString.appendMarkdown(params.toString(), true);
-                        }
-                        if(examples != null){
-                        markdownString.appendMarkdown('_______________________________________________________________');
-                        markdownString.appendMarkdown(examples.toString(), true);
-                        }
-                        markdownString.supportHtml = true;
-                        return new vscode.Hover(markdownString);
-                    })
-                    .catch(error => {
-                        console.error(error);
-                    });
+                const gcodeFile = path.join(__dirname, 'MarlinDocumentation-master', '_gcode', filtered + '.md');
+                return getMarkdownFromFile(gcodeFile);
+              
             }
         }
     });
@@ -159,3 +115,19 @@ function hoverInfoActivate(context) {
 
 
 exports.hoverInfoActivate = hoverInfoActivate;
+
+//Returns markdown from file located in the Marling documentation gcodes folder.
+function getMarkdownFromFile(path){
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                const markdownString = md.render(data);
+                const hoverMarkdownString = new vscode.MarkdownString(markdownString, true);
+                hoverMarkdownString.supportHtml = true;
+                resolve(new vscode.Hover(hoverMarkdownString));
+            }
+        });
+    });
+}
