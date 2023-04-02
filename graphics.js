9353c8d4dd2a54e4c1543046d8bd4933dba32048
ReinisSprogis <69913791+ReinisSprogis@users.noreply.github.com>
2023-04-02 23:33:39 +0100

    Fixed panel bug. Added panel colors
    onDidChangeActiveTextEditor was outside graphics.
    Added randomly colored squares to easily distinguish panels.

--- graphics.js
+++ graphics.js
@@ -6,6 +6,7 @@ const diff = require('fast-diff');
 
 async function graphics(context) {
   const panels = [];
+  const panelColors = {};
 
   const lastGcodeContents = {};
   
@@ -32,6 +33,9 @@ async function graphics(context) {
     const document = editor.document;
     const uri = document.uri.toString();
     const fileName = path.basename(document.fileName);
+    const randomColor = getRandomColor();
+
+
     const panel = vscode.window.createWebviewPanel(
       'toolpathView',
       'Toolpath: ' + fileName,
@@ -40,7 +44,12 @@ async function graphics(context) {
         enableScripts: true,
       }
     );
+    
+    if(panelColors[uri] === undefined) {
+      panelColors[uri] = randomColor;
+    }
 
+    panel.iconPath = getColoredIconUri(panelColors[uri], context);
     panel.uri = uri;
     panels.push(panel);
 
@@ -63,6 +72,8 @@ async function graphics(context) {
         if (panelIndex !== -1) {
           panels.splice(panelIndex, 1);
         }
+        //remove color from panelColors
+        delete panelColors[uri];
       },
       null,
       context.subscriptions
@@ -76,21 +87,45 @@ async function graphics(context) {
       updateToolpath();
     }
   });
+
+  vscode.window.onDidChangeActiveTextEditor((editor) => {
+    if (editor) {
+      const uri = editor.document.uri.toString();
+      console.log(uri);
+      panels.forEach((panel) => {
+        if (panel && panel.webview &&  panel.uri === uri) {
+          panel.uri = uri;
+          panel.webview.postMessage({ type: 'setUri', uri: panel.uri });
+          updateToolpath();
+        }
+      });
+    }
+  });
+
 }
 
-vscode.window.onDidChangeActiveTextEditor((editor) => {
-  if (editor) {
-    const uri = editor.document.uri.toString();
-    console.log(uri);
-    panels.forEach((panel) => {
-      if (panel && panel.webview) {
-        panel.uri = uri;
-        panel.webview.postMessage({ type: 'setUri', uri: panel.uri });
-        updateToolpath();
-      }
-    });
+
+
+
+ 
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
-});
+  return color;
+}
+
+function getColoredIconUri(color, context) {
+  const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+    <rect width="100" height="100" fill="${color}" />
+  </svg>`;
+  const iconUri = vscode.Uri.parse('data:image/svg+xml;base64,' + Buffer.from(svg).toString('base64'));
+  return iconUri;
+}
+
 
 
 
