41e5f7b0cdf46318d14b69a664bb7ecc93910dc3
ReinisSprogis <69913791+ReinisSprogis@users.noreply.github.com>
2023-04-02 23:34:15 +0100

    deprecated for now

--- extension.js
+++ extension.js
@@ -1,12 +1,13 @@
 const { hoverInfoActivate } = require('./hover');
 const { graphics } = require('./graphics');
 const { nodes } = require('./nodes.js');
-
+//const { registerSnippets } = require('./snippets/gcode.js');
 //Bundle file
 function activate(context) {
   console.log('Extension activated!');
   hoverInfoActivate(context);
   graphics(context);
+ // registerSnippets(context);
  // nodes(context);
 }
 
