424bd3238c9b0d5376fe847afd251735ae025485
ReinisSprogis <69913791+ReinisSprogis@users.noreply.github.com>
2023-04-07 21:13:25 +0100

    Extrusion calculation works in absolute, working on extrusion mode detection

--- extension.js
+++ extension.js
@@ -15,44 +15,73 @@ function activate(context) {
       provideCompletionItems(document, position, token, context) {
         const line = document.lineAt(position).text;
         const lineBeforeCursor = line.substr(0, position.character);
-        console.log('providing completion');
         const g1CommandRegex = /^G1\s+((?:F\d+\s*)?(?:X-?\d+(\.\d+)?\s*)?(?:Y-?\d+(\.\d+)?\s*)?(?:Z-?\d+(\.\d+)?\s*)?)*E$/i;
-
+  
         if (g1CommandRegex.test(lineBeforeCursor)) {
           const extrusionCompletion = new vscode.CompletionItem('Extrusion', vscode.CompletionItemKind.Snippet);
-
+  
           const coordsRegex = /X(-?\d+(\.\d+)?)\s*Y(-?\d+(\.\d+)?)/i;
           const coordsMatch = coordsRegex.exec(line);
-
+  
           if (coordsMatch) {
             const x1 = parseFloat(coordsMatch[1]);
             const y1 = parseFloat(coordsMatch[3]);
-            console.log('Extrusion');
-            // Calculate extrusion (E) here based on the coordinates
-            // ...
-            const extrusionValue = 0; // Replace with the actual calculated value
-
+  
+            // Define x2 and y2 as the previous X and Y coordinates
+            // You can update these values based on your requirements
+            const x2 = 0;
+            const y2 = 0;
+  
+            // Get the nozzle diameter and filament diameter from the comments
+            const nozzleDiameterRegex = /;NOZZLE_DIAMETER:(\d+(\.\d+)?)/i;
+            const filamentDiameterRegex = /;FILAMENT_DIAMETER:(\d+(\.\d+)?)/i;
+  
+            let nozzleDiameter = 0.4; // Default nozzle diameter
+            let filamentDiameter = 1.75; // Default filament diameter
+  
+            // Iterate through the lines before the current line and get the most recent nozzle diameter and filament diameter
+            for (let i = 0; i < position.line; i++) {
+              const currentLine = document.lineAt(i).text;
+  
+              const nozzleDiameterMatch = nozzleDiameterRegex.exec(currentLine);
+              const filamentDiameterMatch = filamentDiameterRegex.exec(currentLine);
+  
+              if (nozzleDiameterMatch) {
+                nozzleDiameter = parseFloat(nozzleDiameterMatch[1]);
+              }
+  
+              if (filamentDiameterMatch) {
+                filamentDiameter = parseFloat(filamentDiameterMatch[1]);
+              }
+            }
+  
+            const extrusionValue = calculateExtrusion(x1, y1, x2, y2, nozzleDiameter, filamentDiameter);
             extrusionCompletion.insertText = `E${extrusionValue.toFixed(5)}`;
+            return [extrusionCompletion];
           }
-
-          return [extrusionCompletion];
         }
       },
     },
-    'E' // Add this line to register 'E' as a trigger character
+    'E'
   );
+  
 
   context.subscriptions.push(provider);
 }
 
 
 function calculateExtrusion(x0, y0, x1, y1, layerHeight, lineWidth, filamentDiameter) {
+  if (isNaN(filamentDiameter) || filamentDiameter <= 0) {
+    filamentDiameter = 1.75;
+  }
+
   const distance = Math.sqrt(Math.pow(x1 - x0, 2) + Math.pow(y1 - y0, 2));
   const area = layerHeight * lineWidth;
   const filamentArea = Math.PI * Math.pow(filamentDiameter / 2, 2);
 
   return (area * distance) / filamentArea;
 }
+
 
 function deactivate() {
 }
