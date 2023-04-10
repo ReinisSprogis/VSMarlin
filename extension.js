0b4911c2f3ee599c9096df482e0a3f25eff30f90
ReinisSprogis <69913791+ReinisSprogis@users.noreply.github.com>
2023-04-10 20:42:30 +0100

    Added chart, works/basic ctr+alt+t

--- extension.js
+++ extension.js
@@ -1,87 +1,23 @@
 const { hoverInfoActivate } = require('./hover');
 const { graphics } = require('./graphics');
 const { nodes } = require('./nodes.js');
-const vscode = require('vscode');
+const { timeline } = require('./timeline.js');
+const { extrusionCalculations } = require('./extrusion_calculations.js');
+
 //const { registerSnippets } = require('./snippets/gcode.js');
 //Bundle file
+
 function activate(context) {
   console.log('Extension activated!');
+  //Hover info on commands G and M codes.
   hoverInfoActivate(context);
+  //Toolpath graphics ctrl+shift+r
   graphics(context);
-
-  const provider = vscode.languages.registerCompletionItemProvider(
-    'marlin',
-    {
-      provideCompletionItems(document, position, token, context) {
-        const line = document.lineAt(position).text;
-        const lineBeforeCursor = line.substr(0, position.character);
-        const g1CommandRegex = /^G1\s+((?:F\d+\s*)?(?:X-?\d+(\.\d+)?\s*)?(?:Y-?\d+(\.\d+)?\s*)?(?:Z-?\d+(\.\d+)?\s*)?)*E$/i;
-  
-        if (g1CommandRegex.test(lineBeforeCursor)) {
-          const extrusionCompletion = new vscode.CompletionItem('Extrusion', vscode.CompletionItemKind.Snippet);
-  
-          const coordsRegex = /X(-?\d+(\.\d+)?)\s*Y(-?\d+(\.\d+)?)/i;
-          const coordsMatch = coordsRegex.exec(line);
-  
-          if (coordsMatch) {
-            const x1 = parseFloat(coordsMatch[1]);
-            const y1 = parseFloat(coordsMatch[3]);
-  
-            // Define x2 and y2 as the previous X and Y coordinates
-            // You can update these values based on your requirements
-            const x2 = 0;
-            const y2 = 0;
-  
-            // Get the nozzle diameter and filament diameter from the comments
-            const nozzleDiameterRegex = /;NOZZLE_DIAMETER:(\d+(\.\d+)?)/i;
-            const filamentDiameterRegex = /;FILAMENT_DIAMETER:(\d+(\.\d+)?)/i;
-  
-            let nozzleDiameter = 0.4; // Default nozzle diameter
-            let filamentDiameter = 1.75; // Default filament diameter
-  
-            // Iterate through the lines before the current line and get the most recent nozzle diameter and filament diameter
-            for (let i = 0; i < position.line; i++) {
-              const currentLine = document.lineAt(i).text;
-  
-              const nozzleDiameterMatch = nozzleDiameterRegex.exec(currentLine);
-              const filamentDiameterMatch = filamentDiameterRegex.exec(currentLine);
-  
-              if (nozzleDiameterMatch) {
-                nozzleDiameter = parseFloat(nozzleDiameterMatch[1]);
-              }
-  
-              if (filamentDiameterMatch) {
-                filamentDiameter = parseFloat(filamentDiameterMatch[1]);
-              }
-            }
-  
-            const extrusionValue = calculateExtrusion(x1, y1, x2, y2, nozzleDiameter, filamentDiameter);
-            extrusionCompletion.insertText = `E${extrusionValue.toFixed(5)}`;
-            return [extrusionCompletion];
-          }
-        }
-      },
-    },
-    'E'
-  );
-  
-
-  context.subscriptions.push(provider);
+  //Shows timeline of G and M codes ctrl+shift+t
+  timeline(context);
+  //Calculates node number and time for each G and M code when E is type autocomplete is offered to calculate values for E.
+  extrusionCalculations(context);
 }
-
-
-function calculateExtrusion(x0, y0, x1, y1, layerHeight, lineWidth, filamentDiameter) {
-  if (isNaN(filamentDiameter) || filamentDiameter <= 0) {
-    filamentDiameter = 1.75;
-  }
-
-  const distance = Math.sqrt(Math.pow(x1 - x0, 2) + Math.pow(y1 - y0, 2));
-  const area = layerHeight * lineWidth;
-  const filamentArea = Math.PI * Math.pow(filamentDiameter / 2, 2);
-
-  return (area * distance) / filamentArea;
-}
-
 
 function deactivate() {
 }
---
+++ timeline.js
@@ -0,0 +1,157 @@
+const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
+function timeline(context){
+    console.log('timeline!');
+    const panels = [];
+    const panelColors = {};
+    const lastGcodeContents = {};
+  
+    const updateToolpath = () => {
+      const editor = vscode.window.activeTextEditor;
+      const document = editor.document;
+      const uri = document.uri.toString();
+      const currentGcodeContent = document.getText();
+  
+  
+  
+      if (lastGcodeContents[uri] === undefined || currentGcodeContent !== lastGcodeContents[uri]) {
+        lastGcodeContents[uri] = currentGcodeContent;
+        panels.forEach((panel) => {
+          if (panel && panel.webview && panel.uri === uri) {
+            panel.webview.postMessage({ type: 'updateToolpath', uri, gcodeContent: currentGcodeContent });
+          }
+        });
+      }
+    };
+  
+    let disposable = vscode.commands.registerCommand('marlin.showTimeline', async () => {
+        console.log("Command: marlin.showTimeline");
+        const editor = vscode.window.activeTextEditor;
+        const document = editor.document;
+        const uri = document.uri.toString();
+        const fileName = path.basename(document.fileName);
+        const randomColor = getRandomColor();
+    
+    
+        const panel = vscode.window.createWebviewPanel(
+          'toolpathView',
+          'Toolpath: ' + fileName,
+          vscode.ViewColumn.Three,
+          {
+            enableScripts: true,
+          }
+        );
+    
+        if (panelColors[uri] === undefined) {
+          panelColors[uri] = randomColor;
+        }
+    
+        panel.webview.onDidReceiveMessage(
+          console.log("panel.webview.onDidReceiveMessage"),
+          (message) => {
+            if (message.type === 'ready') {
+              panel.webview.postMessage({ type: 'setUri', uri: panel.uri });
+              updateToolpath();
+            }
+          },
+          undefined,
+          context.subscriptions
+        );
+    
+    
+    
+        //Sets the color for tab icon.
+        //This ir random color squared icon that has the sam color for each panel that is opened from same editor.
+        panel.iconPath = getColoredIconUri(panelColors[uri], context);
+    
+        //Panel uri is used to check if the panel has been created from the same document.
+        //If it is not the same document then the toolpath will not be updated.
+        //This is needed otherwise without tracking what panel is associated with what document the toolpath would be updated for all panels when a change is made in one document.
+        panel.uri = uri;
+        //test: trying to load toolpath wen panel is created
+        panel.webview.postMessage({ type: 'panelOpened', uri: panel.uri });
+        //Add panel to panels array for tracking.
+        panels.push(panel);
+    
+        //Creates panel frm the html file.
+        //In the toolpath.html graphics are managed with three.js and responds to changes in gcode files.
+        //This will further extend to feedback from changes in three.js wil be sent to gcode files. TODO: implement this
+        panel.webview.html = getToolpathHtml();
+    
+    
+        // called when graphics panel is closed
+        // Remove panel from panels array to add new color to panelColors array when new panel is created.
+        panel.onDidDispose(
+          () => {
+            const panelIndex = panels.indexOf(panel);
+            if (panelIndex !== -1) {
+              panels.splice(panelIndex, 1);
+            }
+            //remove color from panelColors
+            delete panelColors[uri];
+          },
+          null,
+          context.subscriptions
+        );
+    });
+
+    context.subscriptions.push(disposable);
+    
+
+  //Updating graphics when a change is made in the active editor.
+  vscode.workspace.onDidChangeTextDocument((event) => {
+    if (event.document === vscode.window.activeTextEditor.document) {
+      console.log('onDidChangeTextDocument');
+      updateToolpath();
+    }
+  });
+
+
+  //This is needed to update correct panel when the active editor changes.
+  //Needed to detect what is current active panel so that when panel focuses to another editor the toolpath is changed in correct panel.
+  vscode.window.onDidChangeActiveTextEditor((editor) => {
+    console.log('onDidChangeActiveTextEditor');
+    if (editor) {
+      const uri = editor.document.uri.toString();
+      console.log(uri);
+      panels.forEach((panel) => {
+        if (panel && panel.webview && panel.uri === uri) {
+          panel.uri = uri;
+          panel.webview.postMessage({ type: 'setUri', uri: panel.uri });
+          updateToolpath();
+        }
+      });
+    }
+  });
+
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+  function getColoredIconUri(color, context) {
+    const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+      <rect width="100" height="100" fill="${color}" />
+    </svg>`;
+    const iconUri = vscode.Uri.parse('data:image/svg+xml;base64,' + Buffer.from(svg).toString('base64'));
+    return iconUri;
+  }
+  
+  
+  
+  
+  
+  function getToolpathHtml() {
+    const toolpathHtml = fs.readFileSync(path.join(__dirname, 'timeline.html'), 'utf-8');
+    return toolpathHtml;
+  }
+  
+
+exports.timeline = timeline;
