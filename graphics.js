1b047cd60fdc433ea906553b4841e36d0956aaee
ReinisSprogis <69913791+ReinisSprogis@users.noreply.github.com>
2023-04-11 19:45:59 +0100

    Graphics following editor on scroll

--- graphics.js
+++ graphics.js
@@ -1,34 +1,47 @@
 const vscode = require('vscode');
 const fs = require('fs');
 const path = require('path');
-const THREE = require('three');
-const diff = require('fast-diff');
 
 async function graphics(context) {
+  console.log('graphics activated!');
   const panels = [];
   const panelColors = {};
-
   const lastGcodeContents = {};
-  
+
   const updateToolpath = () => {
     const editor = vscode.window.activeTextEditor;
     const document = editor.document;
     const uri = document.uri.toString();
     const currentGcodeContent = document.getText();
-  
+    const visibleLines = getVisibleLines(editor);
+
     if (lastGcodeContents[uri] === undefined || currentGcodeContent !== lastGcodeContents[uri]) {
       lastGcodeContents[uri] = currentGcodeContent;
       panels.forEach((panel) => {
         if (panel && panel.webview && panel.uri === uri) {
-          panel.webview.postMessage({ type: 'updateToolpath', uri, gcodeContent: currentGcodeContent });
+          panel.webview.postMessage({ type: 'updateToolpath', uri, gcodeContent: currentGcodeContent, visibleLines });
         }
       });
     }
   };
-  
-  
+
+
+  const sendVisibleLines = () => {
+    const editor = vscode.window.activeTextEditor;
+    const visibleLines = getVisibleLines(editor);
+    const uri = editor.document.uri.toString();
+   console.log("visibleLines: " + visibleLines.start + " - " + visibleLines.end);
+    panels.forEach((panel) => {
+      if (panel && panel.webview && panel.uri === uri) {
+        console.log("send visibleLines to webview");
+        panel.webview.postMessage({ type: 'visibleLines', visibleLines });
+      }
+    });
+  }
+
 
   let disposable = vscode.commands.registerCommand('marlin.showToolpath', async () => {
+    console.log("Command: marlin.showToolpath");
     const editor = vscode.window.activeTextEditor;
     const document = editor.document;
     const uri = document.uri.toString();
@@ -44,16 +57,13 @@ async function graphics(context) {
         enableScripts: true,
       }
     );
-    
-    if(panelColors[uri] === undefined) {
+
+    if (panelColors[uri] === undefined) {
       panelColors[uri] = randomColor;
     }
 
-    panel.iconPath = getColoredIconUri(panelColors[uri], context);
-    panel.uri = uri;
-    panels.push(panel);
-
     panel.webview.onDidReceiveMessage(
+      console.log("panel.webview.onDidReceiveMessage"),
       (message) => {
         if (message.type === 'ready') {
           panel.webview.postMessage({ type: 'setUri', uri: panel.uri });
@@ -64,8 +74,27 @@ async function graphics(context) {
       context.subscriptions
     );
 
+    //Sets the color for tab icon.
+    //This ir random color squared icon that has the sam color for each panel that is opened from same editor.
+    panel.iconPath = getColoredIconUri(panelColors[uri], context);
+
+    //Panel uri is used to check if the panel has been created from the same document.
+    //If it is not the same document then the toolpath will not be updated.
+    //This is needed otherwise without tracking what panel is associated with what document the toolpath would be updated for all panels when a change is made in one document.
+    panel.uri = uri;
+    //test: trying to load toolpath wen panel is created
+    panel.webview.postMessage({ type: 'panelOpened', uri: panel.uri });
+    //Add panel to panels array for tracking.
+    panels.push(panel);
+
+    //Creates panel frm the html file.
+    //In the toolpath.html graphics are managed with three.js and responds to changes in gcode files.
+    //This will further extend to feedback from changes in three.js wil be sent to gcode files. TODO: implement this
     panel.webview.html = getToolpathHtml();
 
+
+    // called when graphics panel is closed
+    // Remove panel from panels array to add new color to panelColors array when new panel is created.
     panel.onDidDispose(
       () => {
         const panelIndex = panels.indexOf(panel);
@@ -78,22 +107,36 @@ async function graphics(context) {
       null,
       context.subscriptions
     );
+
   });
 
   context.subscriptions.push(disposable);
 
+  //Updating graphics when a change is made in the active editor.
   vscode.workspace.onDidChangeTextDocument((event) => {
     if (event.document === vscode.window.activeTextEditor.document) {
+      console.log('onDidChangeTextDocument');
       updateToolpath();
     }
   });
 
+  vscode.window.onDidChangeTextEditorVisibleRanges((e) => {
+
+    if (e.textEditor === vscode.window.activeTextEditor) {
+      console.log('onDidChangeTextEditorVisibleRanges');
+      sendVisibleLines();
+    }
+  });
+
+  //This is needed to update correct panel when the active editor changes.
+  //Needed to detect what is current active panel so that when panel focuses to another editor the toolpath is changed in correct panel.
   vscode.window.onDidChangeActiveTextEditor((editor) => {
+    console.log('onDidChangeActiveTextEditor');
     if (editor) {
       const uri = editor.document.uri.toString();
       console.log(uri);
       panels.forEach((panel) => {
-        if (panel && panel.webview &&  panel.uri === uri) {
+        if (panel && panel.webview && panel.uri === uri) {
           panel.uri = uri;
           panel.webview.postMessage({ type: 'setUri', uri: panel.uri });
           updateToolpath();
@@ -107,7 +150,7 @@ async function graphics(context) {
 
 
 
- 
+
 
 function getRandomColor() {
   const letters = '0123456789ABCDEF';
@@ -125,6 +168,22 @@ function getColoredIconUri(color, context) {
   const iconUri = vscode.Uri.parse('data:image/svg+xml;base64,' + Buffer.from(svg).toString('base64'));
   return iconUri;
 }
+
+
+function getVisibleLines(editor) {
+  const visibleRanges = editor.visibleRanges;
+  const firstVisiblePosition = visibleRanges[0].start;
+  const lastVisiblePosition = visibleRanges[visibleRanges.length - 1].end;
+
+  const firstVisibleLine = firstVisiblePosition.line;
+  const lastVisibleLine = lastVisiblePosition.line;
+
+  return {
+    start: firstVisibleLine,
+    end: lastVisibleLine,
+  };
+}
+
 
 
 
